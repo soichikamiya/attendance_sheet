a95792fab1d338498c6158a01bd7f96e31318bc3
EC2 Default User <ec2-user@ip-172-31-18-110.us-east-2.compute.internal>
2018-08-23 15:59:39 +0000

    勤怠システム環境設定

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
     # 認証トークンをremoteフォームに埋め込む
     config.action_view.embed_authenticity_token_in_remote_forms = true
   end
