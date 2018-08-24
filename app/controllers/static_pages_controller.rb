d1eeb1d0b76acb88b380fe6e49d3ed364e40b6ce
EC2 Default User <ec2-user@ip-172-31-18-110.us-east-2.compute.internal>
2018-08-24 15:56:43 +0000

    勤怠テーブル作成

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,19 @@
 class StaticPagesController < ApplicationController
-  
+
   def home
     if logged_in?
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
+    today = Date.today
+    @first_day = Date.new(today.year, today.month)
+    @end_day = Date.new(today.year, today.month, -1)
+  end
+  
+  def edit
+    today = Date.today
+    @first_day = Date.new(today.year, today.month)
+    @end_day = Date.new(today.year, today.month, -1)
   end
 
   def help
@@ -15,4 +24,5 @@ class StaticPagesController < ApplicationController
   
   def contact
   end
+  
 end
--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,8 @@ module SampleApp
     config.load_defaults 5.1
     # 認証トークンをremoteフォームに埋め込む
     config.action_view.embed_authenticity_token_in_remote_forms = true
+    # 全タイムゾーンをJSTに変更
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
